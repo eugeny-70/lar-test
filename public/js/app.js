c9a5ddb4c50d6d008e62222742604a85d6382c93
Eugeny Khotskiy <eugeny.khockij@gmail.com>
2019-11-19 10:13:12 +0200

    add cabinet page

--- public/js/app.js
+++ public/js/app.js
@@ -36990,8 +36990,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/eugeny/project/laravel5/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/eugeny/project/laravel5/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/lar-test/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/lar-test/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
