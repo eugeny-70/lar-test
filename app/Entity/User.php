a36d6aa226884bc2c0e3d01d39453ae6845fa7b3
Eugeny Khotskiy <eugeny.khockij@gmail.com>
2019-11-20 17:02:36 +0200

    Added User property PHPDoc

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -5,6 +5,13 @@ namespace App\Entity;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $status
+ */
+
 class User extends Authenticatable
 {
     use Notifiable;
